149ba0ec823463efffa78eca96af09367385eb53
Kirthi Krishnamraju <kirthik@microsoft.com>
2012-08-29 13:10:13 -0700

    change 11

--- JabbR/Chat.js
+++ JabbR/Chat.js
@@ -844,7 +844,7 @@
         // Initialize the ui, passing the user preferences
         ui.initialize(state.preferences);
 
-        ui.addMessage('Change 10 ' + originalTitle, 'notification');
+        ui.addMessage('Change 11 ' + originalTitle, 'notification');
         ui.addMessage('Type /help to see the list of commands', 'notification');
 
         connection.hub.start(function () {
